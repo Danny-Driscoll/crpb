bb291cec3bc7aa313c2fb52059149b1937bd5785
Danny-Driscoll <danny.driscoll@datadoghq.com>
2023-09-03 22:20:44 -0400

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const app = express();
+//const tracer = require('dd-trace').init();
+
 
 app.get("/", async (_, res) => {
   console.log("[test] sample log for / route test");
+  console.log("This is Danny's log, with some tags");
   res.status(200).json({"msg":"Hello world!"});
 });
 
